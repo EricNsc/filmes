71474841581c547f3316ec07c4cfcc9efbac2624
Eric <vxa99@hotmail.com>
2024-11-28 16:36:09 -0300

    Movie Card

---
+++ app/components/Card.jsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaStar } from "react-icons/fa";
+
+export default function Card({ result }) {
+  return (
+    <div className="flex flex-col bg-[#1C2541] rounded-lg overflow-hidden w-[300px] h-auto shadow-md shadow-[#1a212b]">
+      <Link href={`/movie/${result.id}`} passHref>
+        <div className="relative w-full" style={{ paddingBottom: '150%' }}>
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${result.poster_path || result.backdrop_path}`}
+            alt={result.title}
+            className="absolute top-0 left-0 w-full h-full object-cover rounded-t-lg"
+          />
+        </div>
+        <div className="w-full p-4 text-white">
+          <h2 className="text-lg font-semibold text-ellipsis whitespace-nowrap overflow-hidden">{result.title}</h2>
+          <div className="flex items-center">
+            <FaStar className="text-yellow-300 mr-1" />
+            <p className="text-sm text-gray-400">{result.vote_average ? result.vote_average.toFixed(1) : 'N/A'}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  )
+}
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -6,12 +6,12 @@ import { CgProfile } from "react-icons/cg";
 export default function Header() {
   return (
     <div className="header-container">
-      <h1 className="recommend">RECOMMEND</h1>
+      <h1 className="recommend">FILMES</h1>
       <Search />
       <div className="profile">
         <span>Perfil</span>
         <div className="profile-icon">
-          <CgProfile size={30} color="#406E8E" />
+          <CgProfile size={40} color="#406E8E" />
         </div> 
       </div>
     </div>
